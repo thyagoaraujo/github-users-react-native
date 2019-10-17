31ab7d221f3c073ab159951d5fffb2edd3f4ef8a
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-10-17 10:41:24 -0300

    config YellowBox ignoreWarnings

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, YellowBox } from 'react-native';
 
 import './src/config/ReactotronConfig';
 
 import Routes from './src/routes';
+
+YellowBox.ignoreWarnings(['Accessing view manager configs']);
 
 export default function App() {
   return (
