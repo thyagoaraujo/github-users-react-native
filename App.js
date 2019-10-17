d201e39fbd32fbf927726192d57e6ceec4a5ee93
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-10-17 10:25:51 -0300

    config react-navigation-stack, config routes

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { View } from 'react-native';
 
 import './src/config/ReactotronConfig';
 
+import Routes from './src/routes';
+
 export default function App() {
-  return <View />;
+  return <Routes />;
 }
